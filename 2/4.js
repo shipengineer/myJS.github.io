3744cc377eaa815a45719089c303bfaf52093607
PoeticCrane <poeticcrane@gmail.com>
2023-09-24 20:52:09 +0300

    changes

--- 2/4.js
+++ 2/4.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /*
 Необязательное задание.
@@ -22,5 +22,42 @@
 Уточнение: Пользователь всегда вводит корректное положительное целое число.
 Подсказка: Возможно, вам понадобится округление чисел и оператор %.
 */
-
-// Здесь пишем решение, данный комментарий необходимо стереть.
+function delimetr() {
+  //получение целого числа
+  let inputNumber = Number(prompt('Введите число на разложение'));
+  //Проверка ввода на отрицательное и что-то, кроме цифр
+  if (Number.isNaN(inputNumber) || inputNumber < 0) {
+    return console.log('Неверное число');
+  }
+  // трансформация в строку
+  let numberStringify = String(inputNumber);
+  // приведение к сотням
+  if (numberStringify.length > 3) {
+    numberStringify = numberStringify.slice(numberStringify.length - 3);
+  }
+  // разложение на десятки сотни ты
+  let thouthands, tens, units;
+  switch (numberStringify.length) {
+    case 3: {
+      thouthands = String(numberStringify).slice(0, 1);
+      tens = String(numberStringify).slice(1, 2);
+      units = String(numberStringify).slice(2);
+      break;
+    }
+    case 2: {
+      tens = String(numberStringify).slice(0, 1);
+      units = String(numberStringify).slice(1);
+      break;
+    }
+    case 1: {
+      units = String(numberStringify).slice(0);
+      break;
+    }
+  }
+  //выведение сообщение
+  console.log(
+    `В числе ${inputNumber} количество сотен: ${thouthands ? thouthands : 0}` +
+      ` десятков: ${tens ? tens : 0}, единиц: ${units ? units : 0}`
+  );
+}
+delimetr();
